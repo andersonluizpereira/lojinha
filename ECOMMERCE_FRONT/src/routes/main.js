a2e127d68308d035cdbb7842448130610a9433fd
andersonluizpereira <andy2903.alp@gmail.com>
2017-05-28 14:06:23 -0300

    loja virtual

---
+++ ECOMMERCE_FRONT/src/routes/main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    return res.render('main/index', {
+        title: 'Ecommerce',
+        layout: 'layouts/base'
+    })
+})
+
+module.exports = router
