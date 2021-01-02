38696c5616659a5f5d390012778fc7523b3a4274
Anderson Pereira <andersononus@hotmail.com>
2021-01-02 17:46:32 -0300

    adjust

--- ecommerce_adm/src/services/auth/login.js
+++ ecommerce_adm/src/services/auth/login.js
@@ -5,20 +5,22 @@ module.exports = (req, res) => {
 		
 				
 		console.log(user)
-		console.log(error)
+		console.log('error '+ error)
 		console.log(req.body.email +" "+req.body.password)
 
 		if (error || user == false) {
-
+            console.log('caiu no erro')
 			return res.redirect('/auth')
 		}
 
 		return req.login(user, (error) => {
 			if (error) {
+				console.log('caiu no reqlogin')
 				return res.redirect('/auth')
 			}
 
 			return res.redirect('/')
 		})
+		return res.redirect('/')
 	})	
 }
